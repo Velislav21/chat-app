e6b04e8f488fd46423b5ffd363186f7f996e1750
Velislav21 <zirovcho34@gmail.com>
2025-05-04 20:37:32 +0300

    added auth middleware and promisifying the jwt sign/verify

---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,30 @@
+import jwt from "../jwt.js";
+
+export default async function authMiddleware(req, res, next) {
+
+    const token = req.headers.authorization?.split(' ')[1];
+    
+    if (!token) {
+        req.isAuthenticated = false;
+        return next();
+    }
+
+    try {
+
+        const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decodedToken;
+        req.isAuthenticated = true;
+        next();
+
+    } catch (err) {
+        req.isAuthenticated = false;
+        next();
+    }
+};
+
+export const isAuth = (req, res, next) => {
+    if (!req.isAuthenticated) {
+        return res.status(401).json({ message: 'Please login!' });
+    }
+    next();
+};
