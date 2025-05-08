272137113ee2b6a82552f31d119686ae3c8eeb1b
Velislav21 <zirovcho34@gmail.com>
2025-05-08 18:48:50 +0300

    displaying the messages for the selected conversation

--- client/src/components/chat/messages/Messages.jsx
+++ client/src/components/chat/messages/Messages.jsx
@@ -9,22 +9,38 @@ import SkeletonLoader from '../../skeleton-loader/SkeletonLoader'
 export default function Messages({ currentConversation }) {
 
     // const { messages: messagesFromContext } = useConversationContext();
-    const { data: messages, isLoading } = useGetMessages();
+    const { data: messages, isFetching } = useGetMessages();
     console.log(messages);
     // console.log(messagesFromContext);
     return (
         <>
-            {true ?
+            {isFetching &&
                 <div className={styles["skeleton-loader-container"]}>
                     <SkeletonLoader />
                     <SkeletonLoader />
                     <SkeletonLoader />
+                    <SkeletonLoader />
+                    <SkeletonLoader />
                 </div>
-                :
+            }
+            {!isFetching && messages.length > 0 &&
                 <div className={styles["chat-container"]}>
                     <h1> <span>To:</span> {currentConversation.fullname}</h1>
                     <div className={styles["messages-container"]}>
-                        <Message />
+
+                        {messages.map((message) => (
+                            <Message key={message._id} message={message} />
+                        ))}
+                        
+                    </div>
+                    <MessageInput />
+                </div>
+            }
+
+            {!isFetching && messages.length === 0 &&
+                <div className={styles["chat-container"]}>
+                    <h1>No messages yet</h1>
+                    <div className={styles["messages-container"]}>
                     </div>
                     <MessageInput />
                 </div>
--- client/src/components/chat/messages/message/Message.jsx
+++ client/src/components/chat/messages/message/Message.jsx
@@ -1,33 +1,26 @@
 import styles from './Message.module.css'
+import useAuthContext from '../../../../hooks/useAuthContext'
+import useConversationContext from '../../../../hooks/useConversationContext'
+export default function Message({ message }) {
 
-export default function Message() {
+    const { user } = useAuthContext();
+    const { currentConversation } = useConversationContext();
+    console.log(currentConversation);
+    console.log(user);
+    console.log(message);
+    const isSender = message.senderId === user._id;
+
     return (
         <>
-            <div className={`${styles["messageRow"]} ${styles["receiverRow"]}`}>
-                <img
-                    className={styles["avatar"]}
-                    alt="Obi-Wan"
-                    src="https://avatar.iran.liara.run/public/girl?username=vilio2"
-                />
-                <div>
-                    <div className={styles["header"]}>
-                        Obi-Wan Kenobi <span className={styles["time"]}>12:45</span>
-                    </div>
-                    <div className={`${styles["message"]} ${styles["receiver"]}`}>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
-                </div>
-            </div>
-
-            <div className={`${styles["messageRow"]} ${styles["senderRow"]}`}>
-                <img
-                    className={styles["avatar"]}
-                    alt="Anakin"
-                    src="https://avatar.iran.liara.run/public/girl?username=vilio2"
-                />
-                <div>
-                    <div className={styles["header"]}>
-                        Anakin <span className={styles["time"]}>12:46</span>
-                    </div>
-                    <div className={`${styles["message"]} ${styles["sender"]}`}>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+            <div className={`${styles["messageRow"]} ${isSender ? styles["senderRow"] : styles["receiverRow"]}`}>
+                <span className={styles["time"]}>{message.createdAt}</span>
+                <div className={styles["message-container"]}>
+                    <img
+                        className={styles["avatar"]}
+                        alt="Obi-Wan"
+                        src={isSender ? user.profilePicture : currentConversation.profilePicture}
+                    />
+                    <div className={`${styles["message"]} ${styles["receiver"]}`}>{message.messageContent}</div>
                 </div>
             </div>
         </>
