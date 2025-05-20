5c45d9b860554049b17decb435828408d6f54cb8
Velislav21 <zirovcho34@gmail.com>
2025-05-20 19:49:52 +0300

    adding hook to listen for when user is deleted to update the other users conversation sidebar

---
+++ client/src/hooks/useListenForDeletedUser.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+import { useQueryClient } from "@tanstack/react-query";
+
+import useAuthContext from "./useAuthContext";
+import useSocketContext from "./useSocketContext";
+
+export default function useListenForDeletedUser() {
+    const { user } = useAuthContext();
+    const { socket } = useSocketContext();
+    const queryClient = useQueryClient();
+
+    useEffect(() => {
+
+        if (!socket || !user) return;
+
+        socket.on("deletedUser", (deletedUserId) => {
+
+            queryClient.setQueryData(['conversations', user._id], (oldUsers) => {
+                return oldUsers.filter(currUser => currUser._id !== deletedUserId);
+            });
+        });
+        return () => {
+            socket.off("deletedUser");
+        };
+    }, [socket, user, queryClient]);
+};
